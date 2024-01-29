66fcd6b95dc3a0192ba3d16538d2e49ee6ba8b05
Bianca Pelegrini <bmpelegrini@gmail.com>
2024-01-29 20:40:40 -0300

    init main async

--- log-error/src/main.rs
+++ log-error/src/main.rs
@@ -1,3 +1,31 @@
+use std::time::{Duration, Instant};
+
+use log::{error, info};
+use service_logger::ServiceLogger;
+
 fn main() {
+    dotenv::dotenv().ok();
+    ServiceLogger::init_from_env();
+
     println!("Hello, world!");
+    let now = Instant::now();
+
+    let tokio_rt = tokio::runtime::Builder::new_multi_thread()
+        .enable_all()
+        .build()
+        .expect("failed to initialize tokio runtime");
+
+    let future = tokio_rt.block_on(async move { main_async().await });
+
+    if let Err(err) = future {
+        error!("{:?}", err);
+        std::thread::sleep(Duration::from_secs(5));
+        std::process::exit(1);
+    }
+
+    info!("total time: {}s", now.elapsed().as_secs());
+}
+
+async fn main_async() -> anyhow::Result<()> {
+    Ok(())
 }
